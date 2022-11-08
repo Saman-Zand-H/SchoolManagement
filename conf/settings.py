5cc94f4db16833778db1bfa4f055e970be1f8a46
DJS <samanzandh84@gmail.com>
2022-11-09 03:03:32 +0330

    bluh bluh

--- conf/settings.py
+++ conf/settings.py
@@ -251,7 +251,7 @@ SITE_ID = 1
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # DjDT confs
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+hostname, _, ips = socket.gethostbyname(socket.gethostname())
 INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + ['127.0.0.1', '10.0.2.2']
 
 # Allauth confs
